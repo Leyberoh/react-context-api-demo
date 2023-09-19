327ad890fe2aa411baada075228c07d73c6043e0
Alexis Parsat-Lacoste <alexis@parsat-lacoste.com>
2023-09-19 14:24:51 +0200

    Implement dark mode with context api

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/theme.context";
+
 function ProjectCard(props) {
-    return (
-      <div className="ProjectCard">
-        <h3>{props.project.name}</h3>
-        <p>{props.project.technologies}</p>
-      </div>
-    );
-  }
-  
-  export default ProjectCard;
-  
+  const dish = useContext(ThemeContext);
+  console.log(dish);
+
+  return (
+    <div className="ProjectCard">
+      <h3>{props.project.name}</h3>
+      <p>{props.project.technologies}</p>
+    </div>
+  );
+}
+
+export default ProjectCard;
